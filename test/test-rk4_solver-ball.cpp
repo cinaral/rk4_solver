e8215fa99c4fd6d84c605484b59f7f908aa27ba6
cinaral <alevent.cinar@gmail.com>
2022-09-25 23:40:42 -0500

    Update CMakeLists.txt

--- test/test-rk4_solver-ball.cpp
+++ test/test-rk4_solver-ball.cpp
@@ -1,7 +1,3 @@
-//* requires input data for verification
-//* test__rk4_solver__ball.m can generate it in ./dat if you have MATLAB (see README.md)
-//* then copy to ./test/dat or use ./scripts/update_test_data.sh
-
 #include "matrix_op.hpp"
 #include "matrix_rw.hpp"
 #include "rk4_solver.hpp"
